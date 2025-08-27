4e72c2407968f86958042065cf215943525e932c
Mayra <mayra.goncalves.santos@escola.pr.gov.br>
2025-08-27 09:42:49 -0300

    Add files via upload

---
+++ 27-08/repetição.js
@@ -0,0 +1,9 @@
+var status = "olá mundo!";
+
+for (let i = 0; i <10; i++){
+    console.log(status)
+};
+
+for (let j = 0; j<10; j++){
+    console.log(j)
+};
---
+++ 27-08/tabuada.js
@@ -0,0 +1,5 @@
+const valor = 2;
+
+for (let i = 1; i < 11; i++){
+    console.log(valor, "x", i, "=", valor *i)
+};
