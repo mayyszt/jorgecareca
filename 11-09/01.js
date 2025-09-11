7f41f30e75bafdfb2b73b44d9604114bdfb27a5a
Mayra <mayra.goncalves.santos@escola.pr.gov.br>
2025-09-11 16:24:11 -0300

    Add files via upload

---
+++ 11-09/01.js
@@ -0,0 +1,7 @@
+let idade = 18;
+if (idade >=18){
+    console.log("pode entrar")
+} 
+else {
+    console.log("não pode entrar")
+};
---
+++ 11-09/02.js
@@ -0,0 +1,9 @@
+let horario = 12;
+
+if (horario < 12) {
+    console.log("bom dia")
+} else if (horario < 20) {
+    console.log("boa tarde")
+} else {
+    console.log("boa noite")
+};
